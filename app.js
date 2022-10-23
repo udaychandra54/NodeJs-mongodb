0e466fee7e4af4a8186d13b1b559b26212c4f67c
uday <udaychandrag@gmail.com>
2022-10-23 09:27:35 +0530

    added cart functionality

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   User.findById("6352b33ac1fdb20932ca7f88")
     .then((user) => {
       console.log(user);
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
--- models/user.js
+++ models/user.js
@@ -1,13 +1,51 @@
 const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
   save() {
     let db = getDb();
     return db.collection("users").insertOne(this);
+  }
+  // add single product
+
+  // addToCart(product) {
+  //   const updatedCart = { items: [{ productId: product._id, quantity: 1 }] };
+  //   let db = getDb();
+  //   return db
+  //     .collection("users")
+  //     .updateOne(
+  //       { _id: new mongodb.ObjectId(this._id) },
+  //       { $set: { cart: updatedCart } }
+  //     );
+  // }
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(
+      (cp) => cp.productId.toString() === product._id.toString()
+    );
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new mongodb.ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
+    const updatedCart = { items: updatedCartItems };
+    let db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
   static findById(userId) {
     let db = getDb();
